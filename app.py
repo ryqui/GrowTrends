8298181b86db38f40b6a3c0e6c8e7660e93c0c25
SeaofChaos <ryan16127@gmail.com>
2023-03-16 00:37:12 -0400

    More UI work

--- app.py
+++ app.py
@@ -11,10 +11,22 @@ class UI(QMainWindow):
     def initUI(self):
         uic.loadUi("UI//dialogue.ui", self)
 
+        #discord file button
         self.discordFileButton = self.findChild(QPushButton, "selectDiscordDataFile")
         self.discordFileLabel = self.findChild(QLabel, "discordDataFileName")
 
         self.discordFileButton.clicked.connect(self.getDiscordFile)
+
+        #item names file button
+        self.itemNamesButton = self.findChild(QPushButton, "selectItemNamesFile")
+        self.itemNamesFileLabel = self.findChild(QLabel, "itemNamesFileName")
+
+        self.itemNamesButton.clicked.connect(self.getItemNamesFile)
+
+        self.rawItemNamesButton = self.findChild(QPushButton, "selectRawItemNamesFile")
+        self.rawItemNamesFileLabel = self.findChild(QLabel, "rawItemNamesFileName")
+
+        self.rawItemNamesButton.clicked.connect(self.getRawItemNamesFile)
 
 
     def getDiscordFile(self):
@@ -23,6 +35,20 @@ class UI(QMainWindow):
         if fileName:
             self.discordFileLabel.setText(os.path.basename(fileName[0]))
         self.discordFileLabel.adjustSize()
+
+    def getItemNamesFile(self):
+        print("Getting item file...")
+        fileName = QFileDialog.getOpenFileName(self, "Open File", "", "JSON file (*.json)")
+        if fileName:
+            self.itemNamesFileLabel.setText(os.path.basename(fileName[0]))
+        self.itemNamesFileLabel.adjustSize()
+    
+    def getRawItemNamesFile(self):
+        print("Getting discord file...")
+        fileName = QFileDialog.getOpenFileName(self, "Open File", "", "All Files (*.*)")
+        if fileName:
+            self.rawItemNamesFileLabel.setText(os.path.basename(fileName[0]))
+        self.rawItemNamesFileLabel.adjustSize()
 
 def window():
     app = QApplication(sys.argv)
