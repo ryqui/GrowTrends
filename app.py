6cd68cf6a7e6e817141b74c4dba47f1ba1b1b0b7
SeaofChaos <ryan16127@gmail.com>
2023-03-17 03:43:21 -0400

    slight UI changes + working with threads
    UI currently NOT functional

--- app.py
+++ app.py
@@ -1,7 +1,31 @@
 from PyQt5 import QtWidgets, uic
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QFileDialog, QPushButton, QApplication
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QFileDialog, QPushButton, QApplication, QProgressBar
+from PyQt5.QtCore import Qt, QThread, pyqtSignal, QObject
+
+import time
 import sys
 import os
+
+from getPrices import startAnalysis, writeToFile
+
+class Worker(QObject):
+    finished = pyqtSignal()
+    #progress = pyqtSignal(int)
+
+    itemNamesFile = None
+    discordFileName = None
+
+    def run(self):
+        #count = 0
+        #while count<100:
+        #    count += 1
+        #    
+        #    time.sleep(0.3)
+        #    self.changeValue.emit(int)
+        self.itemInformation = startAnalysis(self.itemNamesFile, self.discordFileName)
+        self.finished.emit()
+        return False
+
 
 class UI(QMainWindow):
     def __init__(self):
@@ -11,22 +35,32 @@ class UI(QMainWindow):
     def initUI(self):
         uic.loadUi("UI//dialogue.ui", self)
 
+        self.itemNamesFile = None
+        self.discordFileName = None
+        self.workerRunning = False
+
         #discord file button
         self.discordFileButton = self.findChild(QPushButton, "selectDiscordDataFile")
         self.discordFileLabel = self.findChild(QLabel, "discordDataFileName")
-
         self.discordFileButton.clicked.connect(self.getDiscordFile)
 
         #item names file button
         self.itemNamesButton = self.findChild(QPushButton, "selectItemNamesFile")
         self.itemNamesFileLabel = self.findChild(QLabel, "itemNamesFileName")
-
         self.itemNamesButton.clicked.connect(self.getItemNamesFile)
 
         self.rawItemNamesButton = self.findChild(QPushButton, "selectRawItemNamesFile")
         self.rawItemNamesFileLabel = self.findChild(QLabel, "rawItemNamesFileName")
-
         self.rawItemNamesButton.clicked.connect(self.getRawItemNamesFile)
+
+        self.analyzePricesButton = self.findChild(QPushButton, "analyzePricesButton")
+        self.analyzePricesButton.clicked.connect(self.runAnalysis)
+
+        self.saveResultsButton = self.findChild(QPushButton, "saveResults")
+        self.saveResultsButton.clicked.connect(self.saveFile)
+
+        self.progressBar = self.findChild(QProgressBar, "progressBar")
+
 
 
     def getDiscordFile(self):
@@ -35,6 +69,7 @@ class UI(QMainWindow):
         if fileName:
             self.discordFileLabel.setText(os.path.basename(fileName[0]))
         self.discordFileLabel.adjustSize()
+        self.discordFileName = fileName[0]
 
     def getItemNamesFile(self):
         print("Getting item file...")
@@ -42,6 +77,7 @@ class UI(QMainWindow):
         if fileName:
             self.itemNamesFileLabel.setText(os.path.basename(fileName[0]))
         self.itemNamesFileLabel.adjustSize()
+        self.itemNamesFile = fileName[0]
     
     def getRawItemNamesFile(self):
         print("Getting discord file...")
@@ -49,6 +85,39 @@ class UI(QMainWindow):
         if fileName:
             self.rawItemNamesFileLabel.setText(os.path.basename(fileName[0]))
         self.rawItemNamesFileLabel.adjustSize()
+        self.rawItemNamesFile = fileName[0]
+
+    def runAnalysis(self):
+        if self.itemNamesFile and self.discordFileName:
+            self.thread = QThread()
+            self.worker = Worker()
+            self.worker.moveToThread(self.thread)
+
+            self.workerRunning = True
+
+            self.worker.itemNamesFile = self.itemNamesFile
+            self.worker.discordFileName = self.discordFileName
+            
+            self.thread.started.connect(self.worker.run)
+            self.worker.finished.connect(self.thread.quit)
+            self.worker.finished.connect(self.worker.deleteLater)
+            self.thread.finished.connect(self.thread.deleteLater)
+
+            self.thread.start()
+            self.workerRunning = self.thread.join()
+
+    def saveFile(self):
+        name = QFileDialog.getSaveFileName(self, "Save File", "", "CSV Files (*.csv)")
+        writeToFile(self.itemInformation, name[0], 1)
+
+    #def startProgressBar(self):
+        #self.thread = MyThread()
+        #self.thread.changeValue.connect(self.setProgress)
+        #self.thread.start()
+
+    def setProgress(self, val):
+        self.progressBar.setValue(val)
+
 
 def window():
     app = QApplication(sys.argv)
